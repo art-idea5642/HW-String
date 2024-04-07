f0d0f720af14fe2c6b37f2c67610a55c28b825c0
art-idea5642 <chertezhy.ru@gmail.com>
2024-04-07 22:02:10 +0400

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
 
 public class Main {
     public static void main(String[] args) {
-
-        }
+        //task1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println(fullName);
+        //task2
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        //task3
+        fullName = "Иванов Семён Семёнович";
+        String fullNameTrue = fullName.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullNameTrue);
     }
 }
